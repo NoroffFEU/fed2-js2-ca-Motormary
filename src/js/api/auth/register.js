c68f541669a99e20299f3d2386fb5146f20b13dc
Mathias <mat.moen@gmail.com>
2024-08-30 13:10:42 +0200

    feat: register user

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,8 +1,24 @@
-export async function register({
-  name,
-  email,
-  password,
-  bio,
-  banner,
-  avatar,
-}) {}
+import { API_AUTH_REGISTER } from "../constants"
+
+export async function register(data) {
+  
+  try {
+    const response = await fetch(API_AUTH_REGISTER, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+
+    if (response.ok) {
+      alert("Account created successfully")
+    } else {
+      const data = await response.json()
+      console.log(data)
+      alert(data.errors.map((error) => error.message).join("\r\n") + `\r\nstatus code: ${data.statusCode}`)
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
--- src/js/ui/auth/register.js
+++ src/js/ui/auth/register.js
@@ -1,1 +1,17 @@
-export async function onRegister(event) {}
+import { register } from "../../api/auth/register"
+
+export async function onRegister(event) {
+  event.preventDefault()
+
+  const btn = document.querySelector("#registerBtn")
+
+  const formData = new FormData(event.target)
+
+  const formObject = Object.fromEntries(formData.entries())
+
+  console.log(formData)
+
+  btn.setAttribute("disabled", true)
+  await register(formObject)
+  btn.removeAttribute("disabled")
+}
