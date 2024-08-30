5766e8515ff196590f3045bc8dfcbaf44b8bb050
Mathias <mat.moen@gmail.com>
2024-08-30 22:51:31 +0200

    feat(api): read/update profile

--- src/js/api/profile/read.js
+++ src/js/api/profile/read.js
@@ -1,3 +1,25 @@
-export async function readProfile(username) {}
+import { API_OPTIONS, API_SOCIAL_PROFILES } from "../constants"
 
-export async function readProfiles(limit, page) {}
+export async function readProfile(username) {
+    const response = await fetch(`${API_SOCIAL_PROFILES}/${username}`, API_OPTIONS())
+
+    const responseData = await response.json()
+  
+    if (response.ok) {
+      console.log(responseData)
+    } else {
+      handleApiErrors(responseData)
+    }
+}
+
+export async function readProfiles(limit = 12, page = 1) {
+    const response = await fetch(`${API_SOCIAL_PROFILES}?limit=${limit}&page=${page}}`, API_OPTIONS())
+
+    const responseData = await response.json()
+  
+    if (response.ok) {
+      console.log(responseData)
+    } else {
+      handleApiErrors(responseData)
+    }
+}
